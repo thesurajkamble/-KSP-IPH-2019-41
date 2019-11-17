a7cc7651ae2492fe74c87b8f8037c4133ad7e763
surajkamblesk <alphacoderofficial@gmail.com>
2019-11-17 16:38:28 +0530

    Beat Timestamp added

--- League/app/src/main/java/com/deepconnect/beatmanagement/TakePictureActivity.java
+++ League/app/src/main/java/com/deepconnect/beatmanagement/TakePictureActivity.java
@@ -22,7 +22,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -30,42 +32,83 @@ import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.Objects;
 
 public class TakePictureActivity extends AppCompatActivity {
 
-    private StorageReference mStorageRef;
+    StorageReference mStorage;
+    int count = 0;
+    Button button;
+    ImageView imageView;
+    ProgressDialog progressDialog;
 
-    ImageView IV_image;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference EndTime_ref, StartTime_ref;
 
-
-    Button camera_btn;
+    Button camera_btn,camera_btn2;
     Button Upload_btn;
-    private static final int CAMERA_REQUEST_CODE = 1;
+    private static final int GALLERY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
-//        mProgressBar = new ProgressDialog(this);
-        mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        camera_btn = findViewById(R.id.camera_btn);
-        IV_image = findViewById(R.id.IV_image);
+        DatabaseReference Time_ref = database.getReference().child("TimeStamp").child("start");
+
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        progressDialog = new ProgressDialog(this);
 
         camera_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(TakePictureActivity.this, "click on", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                intent.setType("image/*");
-                Toast.makeText(TakePictureActivity.this, "click off", Toast.LENGTH_SHORT).show();
-                startActivityForResult(intent, CAMERA_REQUEST_CODE);
-                finish();
+                count = count + 1;
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+
+                startActivityForResult(intent, GALLERY);
             }
         });
 
     }
 
 
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY) {
+            progressDialog.setMessage("UPLOADLING....");
+            Uri uri = data.getData();
+            progressDialog.show();
+
+            imageView.setImageURI(uri);
+
+                StorageReference fileName = mStorage.child("Photos/" + uri.getLastPathSegment() + ".png");
+
+
+                fileName.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(TakePictureActivity.this, "UPLOAD COMPLETE", Toast.LENGTH_SHORT).show();
+
+                        StartTime_ref.setValue();
+                        progressDialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(TakePictureActivity.this, "FAILED", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
+        }
+    }
+
+
